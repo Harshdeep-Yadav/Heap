ba4052aee500bc1b723d9a11547646565b0d9862
Harshdeep Yadav <harshdeepy5708@gmail.com>
2022-08-28 13:42:33 +0530

    intial commit

--- hand_stand.cpp
+++ hand_stand.cpp
@@ -25,4 +25,21 @@ public:
         return true;
     }
 };
-c++
+// ``````````````````````or````````````````````
+
+ 
+        map<int,int> mp;
+        for(auto it:hand){
+            mp[it]++;  
+        }
+        
+        for(auto it:mp){
+            if(mp[it.first]>0)
+                for(int i=1;i<groupSize;i++){
+                    mp[it.first+i]-=mp[it.first];
+                    if(mp[it.first+i]<0)
+                        return false;
+                }
+        }
+        return true;
+    }
