47366fc347744c4687441412b12067fc69ab6708
Harshdeep Yadav <harshdeepy5708@gmail.com>
2022-08-30 01:12:58 +0530

    intial commit

---
+++ sort_an-array.cpp
@@ -0,0 +1,87 @@
+
+// min heap
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans;
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(auto it: nums){
+            pq.push(it);
+        }
+        while(!pq.empty()){  
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+    }
+};
+
+// sort funtion
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+       sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
+
+
+// merge sort 
+
+class Solution {
+public:
+void merge(vector<int>& nums , int low , int mid , int high)
+ {
+     int i = low ;
+    int j = mid+1;
+    vector<int> temp(high-low+1);
+    int k = 0;
+    
+    while(i<=mid && j<=high)
+    {
+        if(nums[i] <= nums[j])
+        {
+            temp[k++] = nums[i++];
+        }
+        else{
+            temp[k++] = nums[j++];
+        }
+    }
+    while(j <= high)
+    {
+        temp[k++] = nums[j++];
+    }
+    while(i<=mid)
+    {
+        temp[k++] = nums[i++];
+    }
+    k=0 ;
+    for( int i=low ;i<=high ;i++)
+    {
+        nums[i] = temp[k++];
+    }
+}
+
+private:
+void mergesort(vector<int> &nums , int low , int high)
+{
+    if(low>=high)return ;
+    
+    int mid = low +( high-low)/2;
+    mergesort(nums , low , mid);
+    mergesort(nums, mid+1 , high);
+    merge(nums , low , mid, high);
+    
+}
+
+
+public:
+			vector<int> sortArray(vector<int>& nums) {
+			 mergesort(nums, 0, nums.size()-1);
+			return nums;
+    
+}
+};
